2e0963b718b5ac87ecd4bdf80cb3e8930be42f09
odroid <odroid@odroid.com>
2016-02-13 20:33:30 +0000

    refactor code and remove unused code. add helpers.h. add boilerplate opencl code

---
+++ GPU/videofilter/convolve.cl
@@ -0,0 +1,10 @@
+__kernel void convolve(__global const float *image,
+                       const int width, 
+                       const int height,
+                       __global const float *kern,
+                       const int kern_size) 
+{
+    // assuming kernel is square
+    
+}
+                       
--- GPU/videofilter/videofilter.cpp
+++ GPU/videofilter/videofilter.cpp
@@ -6,31 +6,80 @@
 #include "opencv2/opencv.hpp"
 #include <chrono>
 
+#include "helpers.h"
+
 using namespace cv;
 using namespace std;
 
 #define SHOW
+#define STRING_BUFFER_LEN 1024
 
 
 int main(int argc, char** argv)
 {
+    // setup opencl
+    char char_buffer[STRING_BUFFER_LEN];
+    cl_platform_id platform;
+    cl_device_id device;
+    cl_context context;
+    cl_context_properties context_properties[] =
+    { 
+        CL_CONTEXT_PLATFORM, 0,
+        CL_PRINTF_CALLBACK_ARM, (cl_context_properties)callback,
+        CL_PRINTF_BUFFERSIZE_ARM, 0x1000,
+        0
+    };
+    cl_command_queue queue;
+    cl_program program;
+    cl_kernel kernel;
+    int status;
+
+    // initialize OpenCl 
+    clGetPlatformIDs(1, &platform, NULL);
+    clGetPlatformInfo(platform, CL_PLATFORM_NAME, STRING_BUFFER_LEN, char_buffer, NULL);
+    printf("%-40s = %s\n", "CL_PLATFORM_NAME", char_buffer);
+    clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, STRING_BUFFER_LEN, char_buffer, NULL);
+    printf("%-40s = %s\n", "CL_PLATFORM_VENDOR ", char_buffer);
+    clGetPlatformInfo(platform, CL_PLATFORM_VERSION, STRING_BUFFER_LEN, char_buffer, NULL);
+    printf("%-40s = %s\n\n", "CL_PLATFORM_VERSION ", char_buffer);
+
+    context_properties[1] = (cl_context_properties)platform;
+    clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
+    context = clCreateContext(context_properties, 1, &device, NULL, NULL, NULL);
+    queue = clCreateCommandQueue(context, device, 0, NULL);
+
+    // build kernel
+    unsigned char **opencl_program = read_file("convolve.cl");
+    program = clCreateProgramWithSource(context, 1, (const char **)opencl_program, NULL, NULL);
+    if (program == NULL)
+    {
+        printf("Program creation failed\n");
+        return 1;
+    }	
+    int success = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+    if(success != CL_SUCCESS) print_clbuild_errors(program,device);
+    kernel = clCreateKernel(program, "convolve", NULL);
+    printf("Kernel build successful\n");
+
+    // load video
     VideoCapture camera("./bourne.mp4");
     if(!camera.isOpened())  // check if we succeeded
         return -1;
 
+    Size size = Size( (int)camera.get(CV_CAP_PROP_FRAME_WIDTH), (int)camera.get(CV_CAP_PROP_FRAME_HEIGHT) );
+    cout << "SIZE: " << size << endl;
+
+    // Open the output
     const string output_filename = "./output.avi";   // Form the new name with container
     int ex = static_cast<int>(CV_FOURCC('M','J','P','G'));
-    Size S = Size( (int)camera.get(CV_CAP_PROP_FRAME_WIDTH), (int)camera.get(CV_CAP_PROP_FRAME_HEIGHT) );
-    cout << "SIZE: " << S << endl;
-
-    VideoWriter outputVideo;                                        // Open the output
-    outputVideo.open(output_filename, ex, 25, S, true);
-
+    VideoWriter outputVideo;
+    outputVideo.open(output_filename, ex, 25, size, true);
     if (!outputVideo.isOpened())
     {
         cout  << "Could not open the output video for write: " << output_filename << endl;
         return -1;
     }
+
     auto start = chrono::high_resolution_clock::now();
     auto end = chrono::high_resolution_clock::now();
     auto diff = chrono::duration_cast<chrono::milliseconds>(end - start);
@@ -43,34 +92,38 @@ int main(int argc, char** argv)
     waitKey(1);
 #endif
 
+    // make buffer for each frame
+    cl_mem frame_buf_cl = clCreateBuffer(context, CL_MEM_READ_WRITE, size.width * size.height * sizeof(unsigned char), NULL, &status);
+    checkError(status, "Failed to allocate frame buffer");
+
     int max_frames = 299;
     while (true) {
-        Mat cameraFrame, displayframe;
-        count++;
+        if (++count > max_frames) break;
 
-        if(count > max_frames) break;
+        Mat cameraFrame;
         camera >> cameraFrame;
 
-        Mat filterframe = Mat(cameraFrame.size(), CV_8UC3);
-        Mat grayframe,edge_x,edge_y,edge,edge_inv;
+        Mat grayframe, edge_x, edge_y, edge;
         cvtColor(cameraFrame, grayframe, CV_BGR2GRAY);
 
         // do video filter on CPU using OpenCV
         start = chrono::high_resolution_clock::now();
+
         GaussianBlur(grayframe, grayframe, Size(3, 3), 0, 0);
         GaussianBlur(grayframe, grayframe, Size(3, 3), 0, 0);
         GaussianBlur(grayframe, grayframe, Size(3, 3), 0, 0);
-        Scharr(grayframe, edge_x, CV_8U, 0, 1, 1, 0, BORDER_DEFAULT );
-        Scharr(grayframe, edge_y, CV_8U, 1, 0, 1, 0, BORDER_DEFAULT );
-        addWeighted( edge_x, 0.5, edge_y, 0.5, 0, edge );
-        threshold(edge, edge, 80, 255, THRESH_BINARY_INV);
+
+        Scharr(grayframe, edge_x, CV_8U, 0, 1, 1, 0, BORDER_DEFAULT);
+        Scharr(grayframe, edge_y, CV_8U, 1, 0, 1, 0, BORDER_DEFAULT);
+
+        addWeighted( edge_x, 0.5, edge_y, 0.5, 0, edge );  // average between edge_x and edge_y, stored in edge
+        threshold(edge, edge, 80, 255, THRESH_BINARY_INV);  // threshold over 80, all data either 0 or 255
+
         end = chrono::high_resolution_clock::now();
 
-        cvtColor(edge, edge_inv, CV_GRAY2BGR);
         // Clear the output image to black, so that the cartoon line drawings will be black (ie: not drawn).
-        memset((char *)displayframe.data, 0, displayframe.step * displayframe.rows);
-        grayframe.copyTo(displayframe,edge);
-        cvtColor(displayframe, displayframe, CV_GRAY2BGR);
+        Mat displayframe;
+        grayframe.copyTo(displayframe, edge);  // copy with edge as mask
         outputVideo << displayframe;
 
 #ifdef SHOW
